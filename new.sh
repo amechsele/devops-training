6ca123823565dd88aef3fb93ab04b03a6ad79128
Ame Iya <amechsele@yahoo.com>
2021-10-29 11:56:39 -0600

    updated new.sh 291021

--- new.sh
+++ new.sh
@@ -1,6 +1,13 @@
-echo "this is my first file for shell script"
+#!/bin/bash
+for a in 123456
+do
+    if [ $a == 5 ]
+    then
+        sleep 2
+    fi
+    
+    echo "Iteration no $a"
+done
 
-
-edit new.sh on main 16 oct 2021
-edit new.sh ono main again 16 oct 2021
+echo "Hello World"
 
