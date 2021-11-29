0dfc13064f8e4b3ad3c414d58b2eba7572ed312f
Ame Iya <amechsele@yahoo.com>
2021-11-29 16:56:11 -0700

    uploading terraform files

---
+++ ec2.tf
@@ -0,0 +1,33 @@
+provider "aws" {
+  region = "us-east-2"
+}
+
+resource "aws_instance" "dev" {
+  ami           = "ami-0d718c3d715cec4a7"
+  instance_type = var.instancetype
+  key_name      = "amechi"
+  provisioner "remote-exec" {
+    inline = [
+      "sudo amazon-linux-extras install -y nginx1.12",
+      "sudo systemctl start nginx"
+    ]
+    connection {
+      type        = "ssh"
+      host        = self.public_ip
+      user        = "ec2-user"
+      private_key = file("./ohio.pem")
+    }
+  }
+  #instance_type = "t2.micro"
+}
+
+/*
+resource "aws_eip" "lb" {
+    vpc = true
+}
+
+resource "aws_eip_association" "eip_asoc" {
+  instance_id = aws_instance.myec2.id
+  allocation_id = aws_eip.lb.id
+}
+*/
